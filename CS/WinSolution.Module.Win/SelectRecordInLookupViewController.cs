880b0a910b9533ecdf84c70393e8008c85ecb3e2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WinSolution.Module.Win/SelectRecordInLookupViewController.cs
@@ -0,0 +1,19 @@
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.SystemModule;
+
+namespace AuditDemo.Module.Win {
+    public partial class SelectRecordInLookupViewController : ViewController<ListView> {
+        public SelectRecordInLookupViewController() {
+            InitializeComponent();
+            RegisterActions(components);
+        }
+        private void saSelectRecordInLookup_Execute(object sender, SimpleActionExecuteEventArgs e) {
+            SimpleAction processCurrentObjectAction = 
+                Frame.GetController<ListViewProcessCurrentObjectController>().ProcessCurrentObjectAction;
+            if (processCurrentObjectAction.Active.ResultValue 
+                && processCurrentObjectAction.Enabled.ResultValue)
+                processCurrentObjectAction.DoExecute();
+        }
+    }
+}
